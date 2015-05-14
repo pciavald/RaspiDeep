beacb44ae7c70bdf08d70ffe8adbeb1b592ce739
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-05-14 19:45:58 +0200

    init

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pciavald <pciavald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/14 19:10:49 by pciavald          #+#    #+#             */
-/*   Updated: 2015/05/14 19:43:57 by pciavald         ###   ########.fr       */
+/*   Updated: 2015/05/14 19:45:35 by pciavald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int		main(void)
 {
-	gpio_init();
+	//gpio_init();
+	if (map_peripheral(&gpio) == -1)
+	{
+		printf("Failed to map the physical GPIO.\n");
+		return (-1);
+	}
 	gpio_in(4);
 	while (42)
 	{
