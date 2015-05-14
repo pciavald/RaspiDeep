14cff2df04fdf2855c718bdabb98c584ff39b085
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-05-14 20:03:12 +0200

    magnet read

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pciavald <pciavald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/14 19:10:49 by pciavald          #+#    #+#             */
-/*   Updated: 2015/05/14 20:01:21 by pciavald         ###   ########.fr       */
+/*   Updated: 2015/05/14 20:03:05 by pciavald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int		main(void)
 	gpio_in(4);
 	while (42)
 	{
-		printf("%i\n", GPIO_READ(4));
+		if (GPIO_READ(4) == 0)
+			printf("magnet\n");
 	}
 	return 0;
 }
