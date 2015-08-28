0588ff3f033ec0704d2706c7340de46d7e0c93e5
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-08-28 09:38:56 +0200

    fixed watch

--- handheld/watch.sh
+++ handheld/watch.sh
@@ -1,21 +1,9 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    activate.sh                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: pciavald <pciavald@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2015/05/17 06:05:00 by pciavald          #+#    #+#              #
-#    Updated: 2015/08/20 19:58:40 by pciavald         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!/bin/sh
 
-sudo sh -c "echo 1 > /sys/class/leds/led1/brightness"
+echo 1 | sudo tee /sys/class/leds/led1/brightness > /dev/null
 sudo ifup wlan0
 sleep 3
 #check for connection TODO
 mplayer -demuxer lavf "http://192.168.42.1:5001/?action=stream&video_name=stream.mjpg"
-sudo idown wlan0
-sudo sh -c "echo 0 > /sys/class/leds/led1/brightness"
+sudo ifdown wlan0
+echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null
