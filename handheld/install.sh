a8b8d8300e88653f4dc8ee85bd52e4c4c3fd7898
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-08-28 08:32:32 +0200

    lib

--- handheld/install.sh
+++ handheld/install.sh
@@ -75,6 +75,11 @@ BLANK_TIME=0
 BLANK_DPMS=off
 POWERDOWN_TIME=0" | sudo tee /etc/kbd/config > /dev/null
 
+echo "installing libs"
+if [ ! -f /opt/vc/lib/libbcm_host.so ]; then
+	cp $RASPIDEEP/lib/* /opt/vc/lib/
+fi
+
 echo "setting vnc password"
 expect << EOF
 spawn "/usr/bin/vncpasswd"
