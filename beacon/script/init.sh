b55c3ee10aa6f3d01619766580fff81acc5cf7cc
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-08-28 09:01:33 +0200

    argument

--- beacon/script/init.sh
+++ beacon/script/init.sh
@@ -5,4 +5,4 @@ echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null
 echo none >/sys/class/leds/led1/trigger
 echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null
 $RASPIDEEP/script/passive_mode.sh
-$RASPIDEEP/sensor
+$RASPIDEEP/sensor $RASPIDEEP
