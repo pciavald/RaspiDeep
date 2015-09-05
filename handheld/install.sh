bcea2352c6f583485d318c8f3cf8cb003d174bb5
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-05 17:23:47 +0200

    desktop

--- handheld/install.sh
+++ handheld/install.sh
@@ -114,6 +114,11 @@ echo "reducing lxde bar..."
 sed -i "s/autohide=0/autohide=1/" /home/pi/.config/lxpanel/LXDE-pi/panels/panel
 sed -i "s/heightwhenhidden=2/heightwhenhidden=1/" /home/pi/.config/lxpanel/LXDE-pi/panels/panel
 
-echo "hiding trash, setting wallpaper, "
+echo "hiding trash and menus, setting one-click,  setting wallpaper..."
 sed -i "s/show_trash=1/show_trash=0/" /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+sed -i "s/side_pane_mode=dirtree/side_pane_mode=hidden;dirtree/" /home/pi/.config/pcmanfm/LXDE-pi/pcmanfm.conf
+sed -i "s/toolbar=newtab;navigation;home;/toolbar=hidden;newtab;navigation;home;/" /home/pi/.config/pcmanfm/LXDE-pi/pcmanfm.conf
+sed -i "s/show_statusbar=1/show_statusbar=0/" /home/pi/.config/pcmanfm/LXDE-pi/pcmanfm.conf
+sed -i "s/single_click=0/single_click=1/" /home/pi/.config/libfm/libfm.conf
+sed -i "s/auto_selection_delay=600/auto_selection_delay=0/" /home/pi/.config/libfm/libfm.conf
 #TODO set wallpaper
