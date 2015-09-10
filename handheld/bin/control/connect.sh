a94c5112a8a00e7db0b9a7037d9ef59ef1781b22
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-10 18:54:10 +0200

    connect

--- handheld/bin/control/connect.sh
+++ handheld/bin/control/connect.sh
@@ -2,7 +2,7 @@
 
 echo 1 | sudo tee /sys/class/leds/led1/brightness
 sudo ifup wlan0
-while [ ping -c 1 192.168.42.1 ]; do
+while ! ping -c1 192.168.42.1 ; do
 	sudo ifdown wlan0
 	sudo ifup wlan0
 done;
