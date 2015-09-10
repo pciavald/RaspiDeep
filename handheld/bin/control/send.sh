86825490a2220abd0608762fe0876d4f6bfa3dc5
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-10 20:35:16 +0200

    ssh

--- handheld/bin/control/send.sh
+++ handheld/bin/control/send.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 connect.sh
-ssh -o StrictHostKeyChecking=no pi@192.168.42.1 "
+sshpass -praspberry ssh -o StrictHostKeyChecking=no pi@192.168.42.1 "
 rm /home/pi/RaspiDeep/record.sh 2> /dev/null
 $1 > /home/pi/RaspiDeep/record.sh
 chmod 755 /home/pi/RaspiDeep/record.sh"
