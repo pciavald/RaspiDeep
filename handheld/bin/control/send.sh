c53afa6d55baa6a7f045a0bdd694cd8671558110
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-11 21:25:20 +0200

    colons

--- handheld/bin/control/send.sh
+++ handheld/bin/control/send.sh
@@ -2,7 +2,7 @@
 
 connect.sh
 ssh.sh "
-rm /home/pi/RaspiDeep/record.sh 2> /dev/null
-$1 > /home/pi/RaspiDeep/record.sh
-chmod 755 /home/pi/RaspiDeep/record.sh"
+rm /home/pi/RaspiDeep/record.sh 2> /dev/null;
+$1 > /home/pi/RaspiDeep/record.sh;
+chmod 755 /home/pi/RaspiDeep/record.sh;"
 disconnect.sh
