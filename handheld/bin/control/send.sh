0732f4f94f9d742a368e66ebeb287976b1428c1e
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-11 21:36:02 +0200

    echo raspistill

--- handheld/bin/control/send.sh
+++ handheld/bin/control/send.sh
@@ -3,6 +3,6 @@
 connect.sh
 ssh.sh "\
 rm /home/pi/RaspiDeep/record.sh 2> /dev/null;
-$1 > /home/pi/RaspiDeep/record.sh;
+echo \"$1\" > /home/pi/RaspiDeep/record.sh;
 chmod 755 /home/pi/RaspiDeep/record.sh;"
 disconnect.sh
