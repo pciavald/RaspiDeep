b17e457374886f2cf0561cd8d90f9bddb91e7c21
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-11 20:49:37 +0200

    style

--- handheld/bin/control/connect.sh
+++ handheld/bin/control/connect.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo 1 | sudo tee /sys/class/leds/led1/brightness
+echo 1 | sudo tee /sys/class/leds/led1/brightness > /dev/null
 sudo ifup wlan0
--- handheld/bin/control/disconnect.sh
+++ handheld/bin/control/disconnect.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo ifdown wlan0
-echo 0 | sudo tee /sys/class/leds/led1/brightness
+echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null
