7b5a5a7daf6193b195a7a745836d08c585a1c164
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-12 23:40:26 +0200

    not starting tightvncserver

--- handheld/bin/control/init.sh
+++ handheld/bin/control/init.sh
@@ -5,4 +5,3 @@ echo 'out'	| sudo tee /sys/class/gpio/gpio508/direction
 echo '1'	| sudo tee /sys/class/gpio/gpio508/value
 echo 0		| sudo tee /sys/class/leds/led1/brightness
 sudo ifdown wlan0
-tightvncserver &
