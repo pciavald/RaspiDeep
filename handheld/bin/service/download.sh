e9997db36e53e7db206f97f4d43cb8af2f14c9a5
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-13 22:05:22 +0200

    transfering modifs

--- handheld/bin/service/download.sh
+++ handheld/bin/service/download.sh
@@ -3,7 +3,7 @@
 connect.sh
 if ls /dev | grep -q sda1; then
 	mountKey.sh
-	sudo sshpass -praspbery scp -r pi@192.168.42.1:/home/pi/capture* /mnt
+	sshpass -praspbery scp -r pi@192.168.42.1:/home/pi/capture* /mnt
 fi
 disconnect.sh
 pcmanfm /mnt
--- handheld/bin/tools/FORMAT_inserted_key.sh
+++ handheld/bin/tools/FORMAT_inserted_key.sh
@@ -1,7 +1,7 @@
 echo 1 | sudo tee /sys/class/leds/led1/brightness > /dev/null
 if ! df -h | grep sda ; then
 	sudo umount /dev/sda1
-	mkntfs -Q /dev/sda1
+	sudo mkntfs -Q /dev/sda1
 	mountKey.sh
 fi
 echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null
