149c34b73ffef227a0d98a3f0bb3561fe51587b7
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-13 21:13:38 +0200

    mount key before copying

--- handheld/bin/service/download.sh
+++ handheld/bin/service/download.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 connect.sh
-if -e 0 [ df -h | grep sda ]
+if ls /dev | grep -q sda1; then
+	mountKey.sh
 	sudo sshpass -praspbery scp -r pi@192.168.42.1:/home/pi/capture* /mnt
+fi
 disconnect.sh
 pcmanfm /mnt
