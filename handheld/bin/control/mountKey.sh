923e3c9f164703e0f676994de73784061bfda9e5
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-09-13 21:35:52 +0200

    leds

--- handheld/bin/control/mountKey.sh
+++ handheld/bin/control/mountKey.sh
@@ -1,8 +1,10 @@
 KEYDEV=sda1
 
+echo 1 | sudo tee /sys/class/leds/led1/brightness > /dev/null
 if ls /dev | grep -q $KEYDEV; then
 	if df -h | grep $KEYDEV; then
 		sudo umount /dev/$KEYDEV
 	fi
 	sudo mount /dev/$KEYDEV /mnt
 fi
+echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null
--- handheld/bin/control/ssh.sh
+++ handheld/bin/control/ssh.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+echo 1 | sudo tee /sys/class/leds/led1/brightness > /dev/null
 sshpass -praspberry ssh -o StrictHostKeyChecking=no pi@192.168.42.1 $1
+echo 0 | sudo tee /sys/class/leds/led1/brightness > /dev/null
